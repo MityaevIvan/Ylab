614b541be7947e8321e8ed47fca43b9eeaaf46fd
Ivan Mitiaev <ivanmitjaev91@gmail.com>
2022-08-29 11:14:49 +0300

    final solution

--- src/main/java/task1/Task1.java
+++ src/main/java/task1/Task1.java
@@ -5,10 +5,10 @@ import java.util.Random;
 
 public class Task1 {
     public static void main(String[] args) {
-        int [][] array = new int[5][5];
+        int[][] array = new int[5][5];
 
-        for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < array[i].length; j++){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
                 array[i][j] = generateRandomNumber();
             }
         }
@@ -18,7 +18,7 @@ public class Task1 {
         System.out.println("Average value is: " + avgValue(array));
     }
 
-    public static int generateRandomNumber(){
+    public static int generateRandomNumber() {
 
         int result;
         int min = 10;
@@ -26,11 +26,11 @@ public class Task1 {
         return (new Random().nextInt((max - min) + min));
     }
 
-    public static int maxValue(int [][] array){
+    public static int maxValue(int[][] array) {
         int max = 0;
-        for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < array[i].length; j++){
-                if (array[i][j] > max){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] > max) {
                     max = array[i][j];
                 }
             }
@@ -38,11 +38,11 @@ public class Task1 {
         return max;
     }
 
-    public static int minValue(int [][] array){
+    public static int minValue(int[][] array) {
         int min = array[0][0];
-        for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < array[i].length; j++){
-                if (array[i][j] < min){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] < min) {
                     min = array[i][j];
                 }
             }
@@ -50,16 +50,16 @@ public class Task1 {
         return min;
     }
 
-    public static int avgValue(int [][] array){
+    public static int avgValue(int[][] array) {
         int average;
         int sum = 0;
         int n = 25;
-        for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < array[i].length; j++){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
                 sum = sum + array[i][j];
             }
         }
-        average = sum/n;
+        average = sum / n;
         return average;
     }
 }
