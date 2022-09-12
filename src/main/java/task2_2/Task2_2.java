fd1138268d63cd9b34384d4fce2ab4ca166cad6f
Ivan Mityaev <ivanmitjaev91@gmail.com>
2022-09-12 18:58:20 +0300

    task2_2 new solution

--- src/main/java/task2_2/Task2_2.java
+++ src/main/java/task2_2/Task2_2.java
@@ -3,37 +3,19 @@ package task2_2;
 public class Task2_2 {
 
     public static void findSumByNumberInArray(int[] array, int number) {
-        int sum = 0;
-        int n = 0;
-        int m = 1;
-        while (n != m) {
-            sum = array[n] + array[m];
-            if (sum == number) {
-                System.out.println("[" + array[n] + ", " + array[m] + "]");
-                break;
-            }
-            for (int i = m; i < array.length; i++) {
-                sum = array[n] + array[i];
-                if (sum == number) {
-                    System.out.println("[" + array[n] + ", " + array[i] + "]");
-                    return;
+
+        for (int i = 0; i < array.length - 1; i++){
+            for (int j = i + 1; j < array.length; j++){
+                if (array[i] + array[j] == number){
+                    System.out.println("[" + array[i] + ", " + array[j] + "]");
                 }
             }
-            if (sum != number) {
-                n++;
-                m++;
-            }
-            if (m >= 4) {
-                break;
-            }
         }
-        System.out.println("array doesn't contain such pair");
-
     }
 
     public static void main(String[] args) {
         int[] array = {3, 4, 2, 7};
-        findSumByNumberInArray(array, 10);
+        findSumByNumberInArray(array, 9);
     }
 
 }
